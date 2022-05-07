d0fba99d3ac21095bf00f0544547b3c607e37dbf
Gene <gmlyth@gmail.com>
2022-05-07 15:42:46 -0500

    Saving to DynamoDB.

--- src/bot.py
+++ src/bot.py
@@ -42,7 +42,7 @@ async def paywall(ctx, arg):
 	brief="Registers an emoji response.")
 async def emoji(ctx, arg):
     parts = arg.split(':')
-    response = await register_emoji(ctx.guild.name, ctx.guild.id, parts[1], parts[2].replace('>',''))
+    response = await register_emoji(ctx.guild.name, ctx.guild.id, parts[1], int(parts[2].replace('>','')))
     await ctx.channel.send(response)   
     return
 
@@ -65,18 +65,24 @@ async def register_paywall(guild_name, guild_id, paywall_url):
     #result = f"Now we'd add {paywall_url} as a registered paywall."
     obj = {'paywall_url': paywall_url}
     cache.PAYWALLS.append(obj)
+    table = cache.dynamodb_client.Table('PaywallBotPaywall')
+    table.put_item(Item=obj)
     result = f'Added {paywall_url} to paywalls.'
     return result
 
 async def register_emoji(guild_name, guild_id, emoji_name, emoji_id):
     obj = {'guild_name': guild_name, 'guild_id': guild_id, 'emoji_name': emoji_name, 'emoji_id': emoji_id}
     cache.EMOJIS.append(obj)
-    result = f'Added :{emoji_name}: as a registered emjoi response.'
+    table = cache.dynamodb_client.Table('PaywallBotEmoji')
+    table.put_item(Item=obj)   
+    result = f'Added :{emoji_name}: as a registered emoji response.'
     return result
 
 async def register_reply(guild_name, guild_id, reply):
     obj = {'guild_name': guild_name, 'guild_id': guild_id, 'reply': reply}
     cache.REPLIES.append(obj)
+    table = cache.dynamodb_client.Table('PaywallBotReply')
+    table.put_item(Item=obj)    
     result = f'Added :{reply}: as a registered text response.'
     return result
 
--- src/cache.py
+++ src/cache.py
@@ -1,3 +1,7 @@
+import boto3
+
+dynamodb_client = boto3.resource('dynamodb')
+
 PAYWALLS = []
 EMOJIS = []
 REPLIES = []
