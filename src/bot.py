ee9a000085c8731aa9bae61ee975e77903cc6a43
Gene <gmlyth@gmail.com>
2022-05-07 12:34:05 -0500

    Moved stubs of processing record adds to new methods. dynamically processing emojis.

--- src/bot.py
+++ src/bot.py
@@ -29,14 +29,15 @@ async def on_message(message):
 @bot.command(help="This will register the domain of the url that's passed in as a paywall, and ignores it if it's not in the expected format.",
 	brief="Registers a paywall.")
 async def paywall(ctx, arg):
-    response = f"Now we'd add {arg} as a registered paywall."
+    response = await register_paywall(ctx, arg)
     await ctx.channel.send(response)
     return
 
 @bot.command(help="This will register a custom emoji from the server in question as a response to a paywall - just on that server.",
 	brief="Registers an emoji response.")
 async def emoji(ctx, arg):
-    response = f"Now we'd add {arg} as a registered emjoi response for server {ctx.guild.name}."
+    parts = arg.split(':')
+    response = await register_emoji(ctx, parts[1], parts[2].replace('>',''))
     await ctx.channel.send(response)   
     return
 
@@ -46,8 +47,20 @@ async def reply(ctx, *args):
     msg = ""
     for arg in args:
         msg = msg + " " + arg
-    response = f"Now we'd add {msg} as a registered text (or image) response for server {ctx.guild.name}."
+    response = await register_reply(ctx, msg)
     await ctx.channel.send(response)   
     return     
+
+async def register_paywall(ctx, paywall_url):
+    result = f"Now we'd add {paywall_url} as a registered paywall."
+    return result
+
+async def register_emoji(ctx, emoji_name, emoji_id):
+    result = f"Now we'd add {emoji_name} (id:{emoji_id}) as a registered emjoi response for server {ctx.guild.name}."
+    return result
+
+async def register_reply(ctx, reply):
+    result = f"Now we'd add {reply} as a registered text (or image) response for server {ctx.guild.name}."
+    return result
 
 bot.run(DISCORD_TOKEN)
