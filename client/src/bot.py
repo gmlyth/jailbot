d2e6fcbe49b4d863d5fd35003e607bc72fbe320b
Gene <gmlyth@gmail.com>
2022-05-10 13:28:26 -0500

    Added blocked user functionality.

--- client/src/bot.py
+++ client/src/bot.py
@@ -20,6 +20,9 @@ bot = commands.Bot(command_prefix=".")
 async def on_message(message):
     if message.author == bot.user:
         return
+
+    if await is_blocked_user(message.author.id):
+        return
   
     if message.content.startswith('.') == False:
         if await is_paywall(message.content):
@@ -58,14 +61,23 @@ async def reply(ctx, *args):
     await ctx.channel.send(response)   
     return  
 
+@bot.command(help="This block a user from registering paywalls, emojis, or replies (or blocking another user).")
+async def block(ctx, *args):
+    id = ctx.message.mentions[0].id
+    name = ctx.message.mentions[0].display_name
+    response = await register_blocked_user(ctx.guild.name, ctx.guild.id, name, id)
+    await ctx.channel.send(response)
+    return 
+
 @bot.command(help="This will list paywalls, emojis, and replies for the current server.")
 async def paywalls(ctx):
     embed=discord.Embed(title="Paywalls", url="https://github.com/gmlyth/paywallbot"
         , description=f'This is a list of the registered paywall info for {ctx.guild.name}', color=0xFF5733)
     embed.set_author(name=bot.user.display_name, url="https://github.com/gmlyth/paywallbot", icon_url=bot.user.avatar_url)
     paywalls_value = 'There are no paywalls on this server. Type ".paywall (some url) to add one!"'
-    emojis_value = 'There are no emoji responses on this server. Type ".emoji (emoji) to add one!"'
-    replies_value = 'There are no text responses on this server. Type ".reply (reply) to add one!"'
+    emojis_value = 'There are no emoji responses on this server. Type ".emoji to add one!"'
+    replies_value = 'There are no text responses on this server. Type ".reply to add one!"'
+    blocked_users_value = 'There are blocked users on this server. Type ".block to block someone!"'
     if len(cache.PAYWALLS) > 0:
         paywalls_value = ''
         for dict in cache.PAYWALLS:
@@ -73,14 +85,19 @@ async def paywalls(ctx):
     if len(cache.EMOJIS) > 0:
         emojis_value = ''
         for dict in cache.EMOJIS:
-            emojis_value = emojis_value + dict['emoji_name'] + '\n'
+            emojis_value = emojis_value + f':{dict["emoji_name"]}:' + '\n'
     if len(cache.REPLIES) > 0:
         replies_value = ''
         for dict in cache.REPLIES:
-            replies_value = replies_value + dict['reply'] + '\n'            
+            replies_value = replies_value + dict['reply'] + '\n'     
+    if len(cache.BLOCKED_USERS) > 0:
+        blocked_users_value = ''
+        for dict in cache.BLOCKED_USERS:
+            blocked_users_value = blocked_users_value + f'<@{dict["user_id"]}>' + '\n'               
     embed.add_field(name="Paywalls", value=paywalls_value, inline=False)
     embed.add_field(name="Emojis", value=emojis_value, inline=False)
     embed.add_field(name="Replies", value=replies_value, inline=False)
+    embed.add_field(name="Blocked Users", value=blocked_users_value, inline=False)
     await ctx.send(embed=embed)   
     return  
 
@@ -101,20 +118,37 @@ async def register_paywall(guild_name, guild_id, paywall_url):
         result = f'Added {hostname} to paywalls.'
     return result
 
+async def register_blocked_user(guild_name, guild_id, user_name, user_id):
+    if await is_blocked_user(user_id):
+        result = f':<@{user_id}>: is already a blocked user on this server.'
+    else:
+        obj = {'guild_name': guild_name, 'guild_id': guild_id, 'user_name': user_name, 'user_id': user_id}
+        cache.BLOCKED_USERS.append(obj)
+        table = cache.dynamodb_client.Table('PaywallBotBlockedUser')
+        table.put_item(Item=obj)   
+        result = f'<@{user_id}> has been blocked from messing with this bot.'
+    return result
+
 async def register_emoji(guild_name, guild_id, emoji_name, emoji_id):
-    obj = {'guild_name': guild_name, 'guild_id': guild_id, 'emoji_name': emoji_name, 'emoji_id': emoji_id}
-    cache.EMOJIS.append(obj)
-    table = cache.dynamodb_client.Table('PaywallBotEmoji')
-    table.put_item(Item=obj)   
-    result = f'Added :{emoji_name}: as a registered emoji response.'
+    if await is_emoji(emoji_name):
+        result = f':{emoji_name}: is already an emoji response on this server.'
+    else:
+        obj = {'guild_name': guild_name, 'guild_id': guild_id, 'emoji_name': emoji_name, 'emoji_id': emoji_id}
+        cache.EMOJIS.append(obj)
+        table = cache.dynamodb_client.Table('PaywallBotEmoji')
+        table.put_item(Item=obj)   
+        result = f'Added :{emoji_name}: as a registered emoji response.'
     return result
 
 async def register_reply(guild_name, guild_id, reply):
-    obj = {'guild_name': guild_name, 'guild_id': guild_id, 'reply': reply}
-    cache.REPLIES.append(obj)
-    table = cache.dynamodb_client.Table('PaywallBotReply')
-    table.put_item(Item=obj)    
-    result = f'Added "{reply}" as a registered text response.'
+    if await is_reply(reply):
+        result = f'{reply} is already a text response on this server.'
+    else:
+        obj = {'guild_name': guild_name, 'guild_id': guild_id, 'reply': reply}
+        cache.REPLIES.append(obj)
+        table = cache.dynamodb_client.Table('PaywallBotReply')
+        table.put_item(Item=obj)    
+        result = f'Added "{reply}" as a registered text response.'
     return result
 
 #todo: check based on guild id.
@@ -133,6 +167,16 @@ async def is_emoji(emoji):
     
     for dict in cache.EMOJIS:
         if emoji == dict["emoji_name"]:
+            result = True
+            break
+    return result
+
+#todo: check based on guild id.
+async def is_blocked_user(user_id):
+    result = False
+    
+    for dict in cache.BLOCKED_USERS:
+        if user_id == dict["user_id"]:
             result = True
             break
     return result
--- client/src/cache.py
+++ client/src/cache.py
@@ -5,3 +5,4 @@ dynamodb_client = boto3.resource('dynamodb', region_name='us-east-2')
 PAYWALLS = dynamodb_client.Table('PaywallBotPaywall').scan()['Items']
 EMOJIS = dynamodb_client.Table('PaywallBotEmoji').scan()['Items']
 REPLIES = dynamodb_client.Table('PaywallBotReply').scan()['Items']
+BLOCKED_USERS = dynamodb_client.Table('PaywallBotBlockedUser').scan()['Items']
